94a4d52fab89bac80d1c4d4be6b754eb11190ad1
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-08-24 11:58:44 +0300

    Init class component

--- classes/demo/src/App.js
+++ classes/demo/src/App.js
@@ -1,13 +1,19 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>Hello from React!</p>
-            </header>
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>Hello from React!</p>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
