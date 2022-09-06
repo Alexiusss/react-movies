582e97fe13232a512db9fe4bb6f97f0781303bc8
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-09-06 11:20:25 +0300

    Added films list with styles

--- classes/movie-project/src/layout/Main.jsx
+++ classes/movie-project/src/layout/Main.jsx
@@ -1,9 +1,29 @@
-function Main() {
-    return (
-        <main container className="content">
-            Hello from Movies App
-        </main>
-    );
+import React from "react";
+import { Movies } from "../components/Movies";
+
+class Main extends React.Component {
+    state = {
+        movies: [],
+    };
+
+    componentDidMount() {
+        fetch("http://www.omdbapi.com/?apikey=cae4a354&s=matrix")
+            .then((response) => response.json())
+            .then((data) => this.setState({ movies: data.Search }));
+    }
+
+    render() {
+        const { movies } = this.state;
+        return (
+            <main container className="content">
+                {movies.length ? (
+                    <Movies movies={this.state.movies} />
+                ) : (
+                    <h3>Loading...</h3>
+                )}
+            </main>
+        );
+    }
 }
 
 export { Main };
