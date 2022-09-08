372cab36fdede8b26b69a29e33f5239b232f5c22
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-09-08 12:30:58 +0300

    Implemented handling for failed search

--- classes/movie-project/src/layout/Main.jsx
+++ classes/movie-project/src/layout/Main.jsx
@@ -6,35 +6,36 @@ import { Preloader } from "../components/Preloader";
 class Main extends React.Component {
     state = {
         movies: [],
+        loading: true,
     };
 
     componentDidMount() {
         fetch("https://www.omdbapi.com/?apikey=cae4a354&s=matrix")
             .then((response) => response.json())
-            .then((data) => this.setState({ movies: data.Search }));
+            .then((data) =>
+                this.setState({ movies: data.Search, loading: false })
+            );
     }
 
     searchMovies = (str, type = "all") => {
-        console.log(str);
+        this.setState({ loading: true });
         fetch(
             `https://www.omdbapi.com/?apikey=cae4a354&s=${str}${
                 type !== "all" ? `&type=${type}` : ""
             }`
         )
             .then((response) => response.json())
-            .then((data) => this.setState({ movies: data.Search }));
+            .then((data) =>
+                this.setState({ movies: data.Search, loading: false })
+            );
     };
 
     render() {
-        const { movies } = this.state;
+        const { movies, loading } = this.state;
         return (
             <main className="container content">
                 <Search searchMovies={this.searchMovies} />
-                {movies.length ? (
-                    <Movies movies={this.state.movies} />
-                ) : (
-                    <Preloader />
-                )}
+                {!loading ? <Movies movies={movies} /> : <Preloader />}
             </main>
         );
     }
